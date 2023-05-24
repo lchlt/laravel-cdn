a52fbb43e881e84cb59f164c843854f3ded0bb18
Loïc Choulet <loicchoulet@live.fr>
2023-05-24 17:34:04 +0200

    Build asset & custom filesystem public disk url

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('ASSET_URL').'/storage',
+            'url' => env('APP_ENV', 'local') === "local" ? env('APP_URL').'/storage' : env('ASSET_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
